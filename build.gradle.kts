0c40e309bbddf69d70d6b52ce46feaefd82507b0
Ken Murase <lax.potcx1562@gmail.com>
2026-02-03 23:37:31 +0900

    SonarQubeCloudとの戦い

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     // 規格書 (toml) から Kotlin Multiplatform プラグインを読み込む
     // ここ（中央政府）では適用せず、各州に配る準備だけを行う
     alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.org.sonarqube)
 }
 
 val isAtLabo = NetworkInterface.getNetworkInterfaces().asSequence().any { iface ->
@@ -14,6 +15,19 @@ val isAtLabo = NetworkInterface.getNetworkInterfaces().asSequence().any { iface 
 // コマンド実行時に -Prelease=true をつけた時だけ本番モード
 val isRelease = project.hasProperty("release") && project.property("release") == "true"
 val v = rootProject.findProperty("library.version")?.toString() ?: "unspecified"
+
+sonar {
+    properties {
+        property("sonar.projectKey", "kmupdate1_terakoyalabo_library_project")
+        property("sonar.organization", "terakoya-labo")
+        property("sonar.projectName", "Terakoyalabo Library Project")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.token", System.getenv("SONAR_QUBE_CLOUD_TOKEN"))
+
+        // 解析から除外したいファイル（自動生成コードなど）があればここで
+        property("sonar.exclusions", "**/generated/**")
+    }
+}
 
 allprojects {
     group = "jp.terakoyalabo"
@@ -43,7 +57,7 @@ subprojects {
             repositories {
                 maven {
                     val repoType = if (isRelease) "releases" else "snapshots"
-                    val address = if (isAtLabo) rootProject.findProperty("nexus.ip.labonet")?.toString()
+                    val address = if (isAtLabo) rootProject.findProperty("nexus.ip.labonet")?.toString() ?: "192.168.11.6"
                     else rootProject.findProperty("nexus.ip.vpn")?.toString() ?: "100.98.144.29"
 
                     val domain = "http://$address:8081"
